a708e2aa78b874b3c12dd467bd513d2525abb16b
jsruesch <jsruesch@gmail.com>
2020-01-07 13:23:58 -0600

    Rework footer - add logo links, change grid

--- footer.php
+++ footer.php
@@ -1,14 +1,28 @@
 <!-- Footer -->
 <div class="wrapper footer-container">
     <div class="wrapper-content">
-      <section class="pad60">
-        <div class="full">
-          <a href="https://gearlearning.org/" target="_blank">
-            <img class="footer-logo" src="img/gearlearning-alt-dark.svg"/>
-          </a>
+      <section class="grid-2col ggap-30 pad60">
+
+         <div class="grid-3col grid-3col-forcemobile ggap-20">
+
+            <a href="https://gearlearning.org/" target="_blank" class="footer-logo">
+               <img src="img/gearlearning-alt-dark.svg"/>
+            </a>
+         
+            <a href="https://www.wisc.edu/" target="_blank" class="footer-logo">
+               <img src="img/logo-uw.svg"/>
+            </a>
+
+            <a href="https://www.education.wisc.edu/" target="_blank" class="footer-logo">
+               <img src="img/logo-soe.svg"/>
+            </a>
+         </div>
+
+         <div class="full footer-info">
+            <p>Sprocket Data Systems is developed by <a href="https://gearlearning.org/" target="_blank">Gear Learning</a></p>
+            <p class="text-small text-sprocket-slate">© Gear Learning 2019</p>
         </div>
-        <div class="full"><p class="text-center">Sprocket Data Systems is developed by <a href="https://gearlearning.org/" target="_blank">Gear Learning</a></p></div>
-        <div class="full"><p class="text-center text-small text-sprocket-slate">© Gear Learning 2019</p></div>
+
       </section>
     </div>
   </div><!-- END: Footer -->
