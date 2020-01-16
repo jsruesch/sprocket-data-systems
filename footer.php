603f9c7d8f3f6df09d973b958a5dc1c3859901a7
jsruesch <jsruesch@gmail.com>
2020-01-16 09:57:45 -0600

    Address accessibility issues in footer

--- footer.php
+++ footer.php
@@ -5,16 +5,16 @@
 
          <div class="grid-3col grid-3col-forcemobile ggap-20">
 
-            <a href="https://gearlearning.org/" target="_blank" class="footer-logo">
-               <img src="img/gearlearning-alt-dark.svg"/>
+            <a href="https://gearlearning.org/" target="_blank" class="footer-logo" title="Gear Learning">
+               <img src="img/gearlearning-alt-dark.svg" alt="Gear Learning"/>
             </a>
          
-            <a href="https://www.wisc.edu/" target="_blank" class="footer-logo">
-               <img src="img/logo-uw.svg"/>
+            <a href="https://www.wisc.edu/" target="_blank" class="footer-logo"title="University of Wisconsin Madison">
+               <img src="img/logo-uw.svg" alt="University of Wisconsin Madison"/>
             </a>
 
-            <a href="https://www.education.wisc.edu/" target="_blank" class="footer-logo">
-               <img src="img/logo-soe.svg"/>
+            <a href="https://www.education.wisc.edu/" target="_blank" class="footer-logo" title="University of Wisconsin Madison School of Education">
+               <img src="img/logo-soe.svg" alt="University of Wisconsin Madison School of Education"/>
             </a>
          </div>
 
