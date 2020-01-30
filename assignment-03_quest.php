86352c721a13f96e30ab9e9120b12ff7e6ddfe13
jsruesch <jsruesch@gmail.com>
2020-01-30 14:29:53 -0600

    Add Garden legend

--- assignment-03_quest.php
+++ assignment-03_quest.php
@@ -279,47 +279,40 @@
           </div>
           
 
-         <!-- Heatmap Marker Legend : Grocery Store -->
+         <!-- Heatmap Marker Legend : Garden -->
 <div class="full mar20-top">
-   <div class="full mar5-bot"><h4 class="text-sprocket-slate text-extrabold">Heatmap : <span class="text-sprocket-dblue">Grocery Store</span></h4></div>
+   <div class="full mar5-bot"><h4 class="text-sprocket-slate text-extrabold">Heatmap : <span class="text-sprocket-dblue">Garden</span></h4></div>
    <div class="full grid-2col ggap-10 mar10-bot">
       <div class="full box-c mar5-bot">
          <h5 class="text-sprocket-dblue"><span class="legend-marker marker-player-start"></span> Player Start</h5>
       </div>
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-orange-a"></span> Volunteer Opportunity</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-yellow-a"></span> Tai Chi</h5>
       </div>
    </div>
    <div class="full grid-2col ggap-10 mar10-bot">
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-green-a"></span> Cooking Advice Encoutner</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-blue-b"></span> Meal Choice Encounter</h5>
       </div>
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-green-b"></span> Loss Encounter</h5>
-      </div>
-   </div>
-   <div class="full grid-2col ggap-10 mar10-bot">
-      <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-purple-a"></span> Lifting Assistance Encounter</h5>
-      </div>
-      <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-blue-b"></span> Deli</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-magenta-a"></span> Ted</h5>
       </div>
    </div>
    <div class="full grid-2col ggap-10 mar10-bot">
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-magenta-a"></span> Get Together Planning Encounter</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-blue-a"></span> Book Club</h5>
       </div>
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-blue-a"></span> Pharmacy</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-purple-a"></span> Crossword Bench</h5>
+      </div>
+   </div>
+   <div class="full grid-2col ggap-10 mar10-bot">
+      <div class="full box-c mar5-bot">
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-orange-a"></span> Gardening</h5>
       </div>
    </div>         
-   <div class="full grid-2col ggap-10 mar10-bot">
-      <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-yellow-a"></span> News Stand</h5>
-      </div>
-   </div>         
-</div><!-- END: Heatmap Marker Legend : Grocery Store -->
+</div><!-- END: Heatmap Marker Legend : Garden -->
+
 
 
 
--- heatmap-legends/heatmap-legend_garden.php
+++ heatmap-legends/heatmap-legend_garden.php
@@ -6,28 +6,28 @@
          <h5 class="text-sprocket-dblue"><span class="legend-marker marker-player-start"></span> Player Start</h5>
       </div>
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-green-b"></span> Sand Table</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-yellow-a"></span> Tai Chi</h5>
       </div>
    </div>
    <div class="full grid-2col ggap-10 mar10-bot">
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-orange-a"></span> Circle Time Area</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-blue-b"></span> Meal Choice Encounter</h5>
       </div>
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-magenta-a"></span> Kitchen Playset</h5>
-      </div>
-   </div>
-   <div class="full grid-2col ggap-10 mar10-bot">
-      <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-blue-b"></span> Guitar Case</h5>
-      </div>
-      <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-blue-a"></span> Snack Time Area</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-magenta-a"></span> Ted</h5>
       </div>
    </div>
    <div class="full grid-2col ggap-10 mar10-bot">
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-yellow-a"></span> Upset Friend</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-blue-a"></span> Book Club</h5>
+      </div>
+      <div class="full box-c mar5-bot">
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-purple-a"></span> Crossword Bench</h5>
+      </div>
+   </div>
+   <div class="full grid-2col ggap-10 mar10-bot">
+      <div class="full box-c mar5-bot">
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-orange-a"></span> Gardening</h5>
       </div>
    </div>         
 </div><!-- END: Heatmap Marker Legend : Garden -->
