e4e8f32f961879cc7e633a5347c6030b9f3580d4
jsruesch <jsruesch@gmail.com>
2020-01-30 14:14:41 -0600

    Add Casino legend

--- assignment-03_quest.php
+++ assignment-03_quest.php
@@ -279,39 +279,35 @@
           </div>
           
 
-          <!-- Heatmap Marker Legend : Museum -->
+          <!-- Heatmap Marker Legend : Casino-->
 <div class="full mar20-top">
-   <div class="full mar5-bot"><h4 class="text-sprocket-slate text-extrabold">Heatmap : <span class="text-sprocket-dblue">Museum</span></h4></div>
+   <div class="full mar5-bot"><h4 class="text-sprocket-slate text-extrabold">Heatmap : <span class="text-sprocket-dblue">Casino</span></h4></div>
    <div class="full grid-2col ggap-10 mar10-bot">
       <div class="full box-c mar5-bot">
          <h5 class="text-sprocket-dblue"><span class="legend-marker marker-player-start"></span> Player Start</h5>
       </div>
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-yellow-a"></span> Internship Opportunity</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-green-a"></span> Concierge Desk</h5>
       </div>
    </div>
    <div class="full grid-2col ggap-10 mar10-bot">
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-blue-a"></span> Group Photo</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-orange-a"></span> Prize Wheel</h5>
       </div>
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-green-b"></span> Bully Encounter</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-magenta-a"></span> Gift Shop</h5>
       </div>
    </div>
    <div class="full grid-2col ggap-10 mar10-bot">
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-magenta-a"></span> Dino Dig Exhibit</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-yellow-a"></span> Old Friend</h5>
       </div>
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-green-a"></span> Vandalism Encounter</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-blue-a"></span> Skin Care Sample Table</h5>
       </div>
-   </div>
-   <div class="full grid-2col ggap-10 mar10-bot">
-      <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-orange-a"></span> Stairwell Encounter</h5>
-      </div>
-   </div>         
-</div><!-- END: Heatmap Marker Legend : Museum -->
+   </div>      
+</div><!-- END: Heatmap Marker Legend : Casino -->
+
 
 
 
--- heatmap-legends/heatmap-legend_casino.php
+++ heatmap-legends/heatmap-legend_casino.php
@@ -6,28 +6,23 @@
          <h5 class="text-sprocket-dblue"><span class="legend-marker marker-player-start"></span> Player Start</h5>
       </div>
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-green-b"></span> Sand Table</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-green-a"></span> Concierge Desk</h5>
       </div>
    </div>
    <div class="full grid-2col ggap-10 mar10-bot">
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-orange-a"></span> Circle Time Area</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-orange-a"></span> Prize Wheel</h5>
       </div>
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-magenta-a"></span> Kitchen Playset</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-magenta-a"></span> Gift Shop</h5>
       </div>
    </div>
    <div class="full grid-2col ggap-10 mar10-bot">
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-blue-b"></span> Guitar Case</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-yellow-a"></span> Old Friend</h5>
       </div>
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-blue-a"></span> Snack Time Area</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-blue-a"></span> Skin Care Sample Table</h5>
       </div>
-   </div>
-   <div class="full grid-2col ggap-10 mar10-bot">
-      <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-yellow-a"></span> Upset Friend</h5>
-      </div>
-   </div>         
+   </div>      
 </div><!-- END: Heatmap Marker Legend : Casino -->
