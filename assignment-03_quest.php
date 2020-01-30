03826e039d889aee1a237c8c190f4eab8ad331a2
jsruesch <jsruesch@gmail.com>
2020-01-30 14:44:49 -0600

    Add Halloween legend

--- assignment-03_quest.php
+++ assignment-03_quest.php
@@ -279,39 +279,57 @@
           </div>
           
 
-         <!-- Heatmap Marker Legend : Garden -->
+         <!-- Heatmap Marker Legend : Halloween -->
 <div class="full mar20-top">
-   <div class="full mar5-bot"><h4 class="text-sprocket-slate text-extrabold">Heatmap : <span class="text-sprocket-dblue">Garden</span></h4></div>
+   <div class="full mar5-bot"><h4 class="text-sprocket-slate text-extrabold">Heatmap : <span class="text-sprocket-dblue">Halloween</span></h4></div>
    <div class="full grid-2col ggap-10 mar10-bot">
       <div class="full box-c mar5-bot">
          <h5 class="text-sprocket-dblue"><span class="legend-marker marker-player-start"></span> Player Start</h5>
       </div>
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-yellow-a"></span> Tai Chi</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-orange-a"></span> Friend Without Costume</h5>
       </div>
    </div>
    <div class="full grid-2col ggap-10 mar10-bot">
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-blue-b"></span> Meal Choice Encounter</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-yellow-b"></span> Candy Choice</h5>
       </div>
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-magenta-a"></span> Ted</h5>
-      </div>
-   </div>
-   <div class="full grid-2col ggap-10 mar10-bot">
-      <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-blue-a"></span> Book Club</h5>
-      </div>
-      <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-purple-a"></span> Crossword Bench</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-green-b"></span> Barfy Spaceman</h5>
       </div>
    </div>
    <div class="full grid-2col ggap-10 mar10-bot">
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-orange-a"></span> Gardening</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-teal-a"></span> Mummy</h5>
+      </div>
+      <div class="full box-c mar5-bot">
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-yellow-a"></span> Crossing the Street</h5>
+      </div>
+   </div>
+   <div class="full grid-2col ggap-10 mar10-bot">
+      <div class="full box-c mar5-bot">
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-green-a"></span> Spooky House</h5>
+      </div>
+      <div class="full box-c mar5-bot">
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-magenta-b"></span> Firefighter</h5>
       </div>
    </div>         
-</div><!-- END: Heatmap Marker Legend : Garden -->
+   <div class="full grid-2col ggap-10 mar10-bot">
+      <div class="full box-c mar5-bot">
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-magenta-a"></span> Trick or Treat House A</h5>
+      </div>
+      <div class="full box-c mar5-bot">
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-purple-a"></span> Zombie</h5>
+      </div>
+   </div>         
+   <div class="full grid-2col ggap-10 mar10-bot">
+      <div class="full box-c mar5-bot">
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-blue-b"></span> Trick or Treat House B</h5>
+      </div>
+   </div>         
+</div><!-- END: Heatmap Marker Legend : Halloween -->
+
+
 
 
 
--- heatmap-legends/heatmap-legend_halloween.php
+++ heatmap-legends/heatmap-legend_halloween.php
@@ -6,28 +6,44 @@
          <h5 class="text-sprocket-dblue"><span class="legend-marker marker-player-start"></span> Player Start</h5>
       </div>
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-green-b"></span> Sand Table</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-orange-b"></span> Friend Without Costume</h5>
       </div>
    </div>
    <div class="full grid-2col ggap-10 mar10-bot">
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-orange-a"></span> Circle Time Area</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-yellow-b"></span> Candy Choice</h5>
       </div>
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-magenta-a"></span> Kitchen Playset</h5>
-      </div>
-   </div>
-   <div class="full grid-2col ggap-10 mar10-bot">
-      <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-blue-b"></span> Guitar Case</h5>
-      </div>
-      <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-blue-a"></span> Snack Time Area</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-green-b"></span> Barfy Spaceman</h5>
       </div>
    </div>
    <div class="full grid-2col ggap-10 mar10-bot">
       <div class="full box-c mar5-bot">
-         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-yellow-a"></span> Upset Friend</h5>
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-teal-a"></span> Mummy</h5>
+      </div>
+      <div class="full box-c mar5-bot">
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-yellow-a"></span> Crossing the Street</h5>
+      </div>
+   </div>
+   <div class="full grid-2col ggap-10 mar10-bot">
+      <div class="full box-c mar5-bot">
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-green-a"></span> Spooky House</h5>
+      </div>
+      <div class="full box-c mar5-bot">
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-magenta-b"></span> Firefighter</h5>
+      </div>
+   </div>         
+   <div class="full grid-2col ggap-10 mar10-bot">
+      <div class="full box-c mar5-bot">
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-magenta-a"></span> Trick or Treat House A</h5>
+      </div>
+      <div class="full box-c mar5-bot">
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-purple-a"></span> Zombie</h5>
+      </div>
+   </div>         
+   <div class="full grid-2col ggap-10 mar10-bot">
+      <div class="full box-c mar5-bot">
+         <h5 class="text-sprocket-dblue"><span class="legend-marker marker-blue-a"></span> Trick or Treat House B</h5>
       </div>
    </div>         
 </div><!-- END: Heatmap Marker Legend : Halloween -->
